da035e3e196040beedd167c9427de5b3741e6f33
Halie Chmura <halie.chmura@gmail.com>
2021-08-08 00:10:02 -0400

    **chef's kiss** [almost done]

--- tic_tac_toe/game.js
+++ tic_tac_toe/game.js
@@ -1,3 +1,9 @@
+/*
+                            GAME VARIABLES
+ _________________________________________________________________________ 
+ 
+*/
+
 // keeps track of whose turn it is
 let player_x_turn = true;
 
@@ -10,22 +16,35 @@ let player_x_score = 0;
 let player_o_score = 0;
 let num_of_draws = 0; 
 
-// array of table cells 
+// keeps track of if round is over
+let round_over = false;
+
+// array of table cells (used to determine wins/draws)
 let cells = [["avail", "avail", "avail"], 
              ["avail", "avail", "avail"], 
              ["avail", "avail", "avail"]];
 
+
+
+
+/*
+                            FUNCTIONS
+ _________________________________________________________________________ 
+ 
+*/
+
 // called each time a cell is clicked
 function addCell(cellNumber) {
-    // making sure id is a num (note: adding 1 bc 0 is interpreted as falsey)
-    if(parseInt(cellNumber) + 1) {
+    // making sure id is a num (note: adding 1 bc 0 is interpreted as falsey) and round is not over
+    if(parseInt(cellNumber) + 1 && !round_over) {
         addImage(cellNumber);
-        changeTurns();
         checkDraw();
         checkWin(cellNumber);
+        if(!round_over) changeTurns(); 
     }
 }
 
+// changes table cells to portray game correctly
 function addImage(cellNumber) {
     // add image to cell
     var cell = document.getElementById(cellNumber);
@@ -39,46 +58,122 @@ function addImage(cellNumber) {
     cell.id = player_x_turn ? player_x : player_o;
 }
 
+// switches whose turn it is
 function changeTurns() {
     player_x_turn = !player_x_turn;
-    curr_player = player_x_turn ? player_x : player_o;
+    let curr_player = getCurrentPlayer();
     document.getElementById("top-left").innerHTML =  `${curr_player}'s turn`
 }
 
+// determines if a tie has occurred
 function checkDraw() {
     for(let i=0; i<cells.length; i++) {
         if(cells[i].includes("avail")) return;
     }
+    num_of_draws++;
+    round_over=true;
     document.getElementById("top-left").innerHTML="Draw!";
-}
-
-
-function checkWin() {
+    document.getElementById("score-draws").innerHTML=`Draws: ${num_of_draws}`;
 
 }
 
+// determines if a win has occurred
+function checkWin(cellNumber) {
+    if(verticalCheck(cellNumber) || horizontalCheck(cellNumber) || diagonalCheck()) {
+        round_over = true;
+        let current_player = getCurrentPlayer();
+
+        var current_player_score= player_x_turn ? player_x_score++ : player_o_score++;
+        current_player_score++;
+
+        document.getElementById("top-left").innerHTML=`${current_player} wins!`;
+        document.getElementById(`score-${current_player}`).innerHTML=`${current_player}'s score: ${current_player_score}`;
+    }
+}
+
+// checks to see if there is a row win
+function horizontalCheck(cellNumber) {
+    var col_num = parseInt(cellNumber[0]);
+    var curr_player = getCurrentPlayer();  
+    console.log(curr_player);
+    for(let i=0; i<cells.length; i++) {
+        if(curr_player !== cells[col_num][i]) return false;
+    }
+    return true;
+}
+
+// checks to see if there is a column win
+function verticalCheck(cellNumber) {
+    var row_num = parseInt(cellNumber[1]);
+    var curr_player = getCurrentPlayer(); 
+    for(let i=0; i<cells.length; i++) {
+        if(curr_player !== cells[i][row_num]) return false;
+    }
+    return true;
+}
+
+// checks two diagonals to see if win has occurred (hardcoded)
+function diagonalCheck() {
+    var curr_player = getCurrentPlayer(); 
+    return((cells[0][0] === curr_player && cells[1][1] === curr_player && cells[2][2] === curr_player)
+    || (cells[0][2] === curr_player && cells[1][1] === curr_player && cells[2][0] === curr_player)
+    );
+}
 
 
+// allows for next round to occur
 function nextRound() {
     // blur entire screen
     document.getElementById("will-blur").classList.add("blurred");
-    
     // get box to pop up to indicate if next round should start
+    document.getElementById("popup-box").classList.remove("d-none");
 }
 
-function nextRoundyYes() {
-    // erase all images from srcs
+// user hits 'yes' for going to next round
+function nextRoundYes() {
+    // reset all image paths of srcs
+    let game_board = document.getElementsByClassName('game-table');
+    let all_images = game_board[0].getElementsByTagName('img');
+    [...all_images].forEach(image => image.src="");
 
     // reset double array cells
+    //cells.forEach(arr => arr.forEach(cell => cell="avail"));
+    cells = [["avail", "avail", "avail"], 
+             ["avail", "avail", "avail"], 
+             ["avail", "avail", "avail"]];
 
+    // reset all cell ids
+    let all_tds = document.getElementsByTagName('td');
+    var curr = 0;
+    for(let i=0; i<cells.length; i++) {
+        for(let j=0; j<cells.length; j++) {
+            all_tds[curr].id = `${i}${j}`;
+            curr++;
+        }
+    }
+
+    // reset variables
+    round_over=false;
+    document.getElementById("top-left").innerHTML =  `${getCurrentPlayer()}'s turn`
+    
     // unblur entire screen
-    document.getElementsByClassName("container")[0].classList.remove("blurred");
+    document.getElementById("will-blur").classList.remove("blurred");
 
+    // remove pop-up box
+    document.getElementById("popup-box").classList.add("d-none");
 }
 
+// user hits 'no' for going to next round
 function nextRoundNo() {
     // unblur entire screen
-    document.getElementsByClassName("container")[0].classList.remove("blurred");
+    document.getElementById("will-blur").classList.remove("blurred");
+
+    // remove pop-up box
+    document.getElementById("popup-box").classList.add("d-none");
+}
+
+function getCurrentPlayer() {
+    return player_x_turn ? player_x : player_o;
 }
 
 
